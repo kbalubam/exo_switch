7fd298bd2d14f86e4dd6364a77c140b6cd2dcd49
kbalubam <kevin.baluba@hotmail.com>
2021-05-10 19:29:40 +0200

    vf

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+let jour = prompt("Quel jour de la semaine sommes-nous? ")
+
+switch (jour) {
+    case "Lundi":
+        alert("Nous sommes lundi")
+        break;
+    case "Mardi":
+        alert("Nous sommes Mardi ")
+        break;
+    case "Mercredi":
+        alert("Nous sommes Mercredi")
+        break;
+    case "Jeudi":
+        alert("Nous sommes Jeudi")
+        break;
+    case "Vendredi":
+        alert("Nous sommes Vendredi")
+        break;
+    case "Samedi":
+        alert("Nous sommes Samedi")
+        break;
+    case "Dimanche":
+        alert("Nous sommes Dimanche")
+        break;
+    default:
+        break;
+}
